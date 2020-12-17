99c1be826984cc3513eb2f0aadfb28e3d334dcde
Julio Gamarra <jgamarra@fi.uba.ar>
2020-12-17 14:51:29 -0300

    agregado ejercicio 8

--- Arreglos/ejercicio8.c
+++ Arreglos/ejercicio8.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int completar_ceros(int arreglo[], int n) {
     for(size_t i = 0; i < n; i++) {
@@ -22,14 +24,22 @@ int suma(int arreglo[], int n) {
 
 
 int media(int arreglo[], int n) {
-
     int media = suma(arreglo, n) / n;
     return media;
+}
+
+float varianza(int arreglo[], int n) {
+    int total = 0;
+    for(size_t i = 0; i < n; i++) {
+        total += pow(arreglo[i] - media(arreglo,n), 2);
+    }
+    float varianza = ((float)1 / n) * total;
+    return varianza;
 }
 
 int main() {
     int mi_arreglo[5] = {1,2,3,4,5};
 
-    printf("La suma es: %d\nla media es: %d\n", suma(mi_arreglo, 5), media(mi_arreglo, 5));
+    printf("La suma es: %d\nla media es: %d\nla varianza es: %f\n", suma(mi_arreglo, 5), media(mi_arreglo, 5), varianza(mi_arreglo, 5));
     return 0;
 }
